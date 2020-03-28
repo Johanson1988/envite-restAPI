55555578431deb2f8c79ebf04f473677a2cc9db7
Johann <johanson88@gmail.com>
2020-03-29 00:16:45 +0100

    Add card Model

---
+++ models/Card.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+    palo: {type:String, enum:['oros','bastos', 'copas', 'espadas'], required: true},
+    valor: {type: Number, enum:[1,2,3,4,5,6,7,8,9,10], required: true},
+    photoUrl: {type:String},
+    created_by: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+  }, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+  });
+  
+  const Question = mongoose.model('Question', questionSchema);
+  
+  module.exports = Question;
