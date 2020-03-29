e4001672e6cea2a75d42325b5ad3bb843f74ef12
Johann <johanson88@gmail.com>
2020-03-29 01:50:44 +0100

    Add chicoID to chat Message Model

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const chatMessageSchema = new Schema({
     message: { type: String, required: true},
     gameId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true},
+    chicoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chico', required: true},
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     generalChat: { type: Boolean, required: true}
   }, {
