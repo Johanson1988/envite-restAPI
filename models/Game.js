b98fadc66b3cee773be1ff3b00e2b4fc8c67eeea
Johann <johanson88@gmail.com>
2020-03-29 01:45:52 +0100

    Add Chico, Game Rename Movement

--- models/Game.js
+++ models/Game.js
@@ -4,17 +4,17 @@ const Schema = mongoose.Schema;
 const gameSchema = new Schema({
     name: {type:String, required:true},
     //qrCode: {type:String},
-    movementsList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movements'}],
-    players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
     winners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+    loosers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true},
     gamesToWin: { type: Number, required: true},
     gameStatus: { type:String, enum:['waitingForPlayers','Running','Finished'], required:true},
     chatLog: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage'}],
-    stonesTeamA: [{ type: Number, required: true}],
-    stonesTeamB: [{ type: Number, required: true}],
-    gamesTeamA: [{ type: Number, required: true}],
-    gamesTeamB: [{ type: Number, required: true}],
+    scoreTeamA: [{ type: Number, required: true}],
+    scoreTeamB: [{ type: Number, required: true}],
+    chicos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chico'}],
   }, {
     timestamps: {
       createdAt: 'created_at',
