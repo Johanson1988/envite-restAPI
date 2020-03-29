37de00b1a93920a77970f0342e6f56518e8db097
Johann <johanson88@gmail.com>
2020-03-29 01:24:16 +0100

    add ChatMessage Model

---
+++ models/ChatMessage.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chatMessageSchema = new Schema({
+    message: { type: String, required: true},
+    gameId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true},
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    generalChat: { type: Boolean, required: true}
+  }, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+  });
+  
+  const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
+  
+  module.exports = ChatMessage;
