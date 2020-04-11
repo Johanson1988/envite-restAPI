d18df6dfeffade8bb8c918ecbf420548ec937d5a
Johann <johanson88@gmail.com>
2020-04-11 04:19:12 +0200

    socket auth working chat working

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const createError = require('http-errors');
+const cors = require('cors');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
+// EXPRESS SERVER INSTANCE
+const app = express();
 
-var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// MONGOOSE CONNECTION
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true    
+  })
+  .then( () => console.log(`Connected to database`))
+  .catch( (err) => console.error(err));
+
+// CORS MIDDLEWARE SETUP
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.PUBLIC_DOMAIN,'http://127.0.0.1'],
+  }),
+);
+
+// SESSION MIDDLEWARE
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
+// MIDDLEWARE
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+// ROUTER MIDDLEWARE
+const router = require('./routes/index');
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -23,12 +68,16 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.error(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
