292b02d8064b5dddd3a725d76eba59d7b750b40e
Johann <johanson88@gmail.com>
2020-04-12 17:36:44 +0200

    add auth

---
+++ routes/auth.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./../models/Player');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const createError = require('http-errors');
+
+// HELPER FUNCTIONS
+const {
+    isLoggedIn,
+    isNotLoggedIn,
+    validationLoggin,
+  } = require('./../helpers/middleware');
+
+  //  POST    '/login'
+router.post('/login', isNotLoggedIn, validationLoggin, async (req, res, next) => {  
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username }) ;
+    if (!user) {
+      next(createError(404));
+    } 
+    else if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res
+        .status(200)
+        .json(user);
+      return 
+    } 
+    else {
+      next(createError(401));
+    }
+  } 
+  catch (error) {
+    next(error);
+  }
+},
+);
+
+//  POST    '/signup'
+router.post('/signup', isNotLoggedIn, validationLoggin, async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const usernameExists = await User.findOne({ username }, 'username');
+    
+    if (usernameExists) return next(createError(400));
+    else {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashPass = bcrypt.hashSync(password, salt);
+      const newUser = await User.create({username, password: hashPass });
+      req.session.currentUser = newUser;
+      res
+        .status(200)  //  OK
+        .json(newUser);
+    }
+  } 
+  catch (error) {
+    next(error);
+  }
+},
+);
+//		routes/auth.js
+
+//  POST    '/logout'
+router.post('/logout', isLoggedIn, (req, res, next) => {
+  req.session.destroy();
+  res
+    .status(204)  //  No Content
+    .send();
+  return; 
+});
+
+module.exports = router;  
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/auth', require('./auth'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/public/index.html');
