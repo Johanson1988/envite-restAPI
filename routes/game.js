29569bed533eba30904dbfe5f3f1981652717b5d
Johann <johanson88@gmail.com>
2020-04-12 20:13:31 +0200

    Add new route GET /game -> get game by id

--- routes/game.js
+++ routes/game.js
@@ -9,11 +9,22 @@ router.post('/', (req, res, next) => {
     console.log(roundtime, numberOfPlayers, createdBy, gameStatus);
     Game.create({roundtime, numberOfPlayers, createdBy, gameStatus})
         .then( newGame => {
-            console.log('escrbiendo BBDD')
             const { _id } = newGame;
             res.status(201).json(_id);
         })
         .catch(err => res.status(422).send());
+});
+
+router.get('/', (req, res, next) => {
+    const Game = require('./../models/Game');
+    const { _id } = req.body;
+
+    Game.findOne({_id})
+        .then(gameFound => {
+            if (!gameFound) throw Error;
+            else res.status(202).json(gameFound)
+        })
+        .catch(err => res.status(404).send())
 });
 
 module.exports = router;
