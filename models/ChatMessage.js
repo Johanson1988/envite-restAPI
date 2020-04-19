9bc80b91318dd54619789c9bd9374d6fbc56c8ac
Johann <johanson88@gmail.com>
2020-04-19 21:52:28 +0200

    save sent messages in user socket

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -4,9 +4,7 @@ const Schema = mongoose.Schema;
 const chatMessageSchema = new Schema({
     message: { type: String, required: true},
     gameId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true},
-    chicoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chico', required: true},
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true},
-    generalChat: { type: Boolean, required: true}
   }, {
     timestamps: {
       createdAt: 'created_at',
