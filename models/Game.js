83b9a938b30408b0a9b7b97acadebc3ce88a899c
Johann <johanson88@gmail.com>
2020-04-25 21:54:25 +0200

    add start-game

--- models/Game.js
+++ models/Game.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({    
-    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
-    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
-    winners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
-    loosers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+    winners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+    loosers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true},
     gamesToWin: { type: Number},
-    gameStatus: { type:String, enum:['waitingForPlayers','Running','Finished'], required: true},
-    chatLog: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage'}],
-    scoreTeamA: [{ type: Number}],
-    scoreTeamB: [{ type: Number}],
-    chicos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chico'}],
+    gameStatus: { type:String, enum:['waitingForPlayers','Running','Finished' ], required: true},
+    chatLog: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage' }],
+    scoreTeamA: [{ type: Number }],
+    scoreTeamB: [{ type: Number }],
+    chicos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chico' }],
     numberOfPlayers: { type: Number, enum: [4, 6, 8, 10], required: true },
-    roundTime: { type: String, enum: ['30s', '60s', '90s', '180s', 'no-limit']},
-    table: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}]
+    roundTime: { type: String, enum: ['30s', '60s', '90s', '180s', 'no-limit' ]},
+    table: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }]
   }, {
     timestamps: {
       createdAt: 'created_at',
