a71b7bf097ae73212ef68d2062a7268407d531bb
Johann <johanson88@gmail.com>
2020-05-10 18:07:48 +0200

    temp commit

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,14 +2,31 @@
 
 exports.setGameStatus = (game, status) => game.status = status;
 
+exports.checkGameReady = game => {
+    if (game.numberOfPlayers === game.table.length && game.teamA.length === game.teamB.length && game.status === 'waitingForPlayers' || game.status === 'Ready') return true
+    else return false;
+}
+
 //Socket helpers
 
-exports.sendGameData = (io, socket, game) => {
-    if (game.numberOfPlayers === game.table.length && game.teamA.length === game.teamB.length && game.status === 'waitingForPlayers') game = this.setGameStatus(game, 'Ready');
-
-    io.of('/').in(game._id).clients((error, data)=> {
+exports.sendGameData = (io, socket) => {
+    const { game } = socket;
+    io.of('/').in(game._id).clients((error, clients)=> {
         if (error) throw error;
-        data.forEach(client => io.sockets.connected[client].game.table = game.table);
+        clients.forEach(client => {io.sockets.connected[client].game = socket.game});
         io.to(socket.id).emit('send-game-data', socket.game);
     });
 };
+
+exports.getUpdatedGame = (io,socket) => {
+    const { game } = socket;
+    console.log('game dentro de updated game', game)
+    io.of('/').in(game._id).clients((error, clients)=> {
+        if (error) throw error;
+        console.log(clients);
+        console.log(io.sockets.connected[clients[0]].game);
+        const updatedGame = io.sockets.connected[clients[0]].game ? io.sockets.connected[clients[0]].game : game;
+        console.log('justo después de actualizr', updatedGame);
+        return updatedGame;
+    });
+}
--- models/Game.js
+++ models/Game.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({    
-    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-    winners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-    loosers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true},
+    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    winners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    loosers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     gamesToWin: { type: Number},
     gameStatus: { type:String, enum:['waitingForPlayers', 'Ready','Running','Finished' ], required: true},
     chatLog: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage' }],
@@ -15,7 +15,7 @@ const gameSchema = new Schema({
     chicos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chico' }],
     numberOfPlayers: { type: Number, enum: [4, 6, 8, 10], required: true },
     roundTime: { type: String, enum: ['30s', '60s', '90s', '180s', 'no-limit' ]},
-    table: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }]
+    table: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
   }, {
     timestamps: {
       createdAt: 'created_at',
